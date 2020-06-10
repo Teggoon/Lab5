60944c5b8106296c31b75fb4dddc5eda9a6ec7ec
Haocheng Li <daniel.hc.li@gmail.com>
2020-06-09 19:08:27 -0700

    step 1

--- Sim/top_level5_sol.sv
+++ Sim/top_level5_sol.sv
@@ -31,7 +31,7 @@ module top_level5_sol(
 				  km_loop, 
 				  kl;
 	initial begin
-	 step = 0;
+	 step = 1;
 	 jl = 0;
 	 kl = 0;
 	 mm = 0;
@@ -43,7 +43,7 @@ module top_level5_sol(
 	end
 
   dat_mem dm1(.clk(clk), .write_en(wr_en), .raddr(raddr), .waddr(waddr), // memory_connection
-       .data_in(data_in), .data_out(data_out));                   // instantiate data memory
+       .data_in(data_out), .data_out(data_in));                   // instantiate data memory
 		 
 		 
 	  always @(posedge clk) begin  //:clock_loop
@@ -51,18 +51,35 @@ module top_level5_sol(
     if(!init)
 	  cycle_ct <= cycle_ct + 1;
     if(!init && initQ) begin //:init_loop  // falling init
+		
+    end //:init_loop
+	 begin // loop 2
+	 
 	  begin  //:loop2
 		 if (step == 1) begin
+			$display("Currently at step 1.");
 			//read address should be 64+jl
 			if (alternate_1 == 0) begin
-			raddr <= 64+jl;
-			alternate_1 = 1;
+			
+			// number to go into raddr
+			$display("Num: %h", 8'b1000000 + jl);
+			
+			// modify raddr
+			raddr <= 8'b1000000 + jl;
+			
+			// this is different hmm
+			$display("Raddr: %h", raddr);
+			alternate_1 <= 1;
 			end
 			else begin // alernate_1 = 1
-			LFSR[jl] <= data_out ^ 6'h1f;
+			$display("data_in: %h", data_in);
+			LFSR[jl] <= data_in ^ 6'h1f;
 			alternate_1 <= 0;
 				if (jl < 7) jl <= jl + 1;
-				else			 step <= 2;
+				else begin
+					step <= 2;
+					raddr = 64;
+				end
 			end
 		  /*
 	      LFSR[jl] =         dm1.core[64+jl][5:0]^6'h1f;
@@ -76,13 +93,12 @@ module top_level5_sol(
 		  end // end of step 1
 		  else if (step == 2) begin
 		  
-				raddr = 64;
-				lfsr_trial[0][0] = data_out^6'h1f;
-				lfsr_trial[1][0] = data_out^6'h1f;
-				lfsr_trial[2][0] = data_out^6'h1f;
-				lfsr_trial[3][0] = data_out^6'h1f;
-				lfsr_trial[4][0] = data_out^6'h1f;
-				lfsr_trial[5][0] = data_out^6'h1f;
+				lfsr_trial[0][0] <= data_in^6'h1f;
+				lfsr_trial[1][0] <= data_in^6'h1f;
+				lfsr_trial[2][0] <= data_in^6'h1f;
+				lfsr_trial[3][0] <= data_in^6'h1f;
+				lfsr_trial[4][0] <= data_in^6'h1f;
+				lfsr_trial[5][0] <= data_in^6'h1f;
 				step <= 3;
 		  end
 		  
@@ -90,12 +106,12 @@ module top_level5_sol(
 				
 //          $display("trial 0 = %h",lfsr_trial[0][0]);
           for(int kl=0;kl<6;kl++) begin //:trial_loop
-            lfsr_trial[0][kl+1] = (lfsr_trial[0][kl]<<1)+(^(lfsr_trial[0][kl]&LFSR_ptrn[0]));   
-            lfsr_trial[1][kl+1] = (lfsr_trial[1][kl]<<1)+(^(lfsr_trial[1][kl]&LFSR_ptrn[1]));   
-            lfsr_trial[2][kl+1] = (lfsr_trial[2][kl]<<1)+(^(lfsr_trial[2][kl]&LFSR_ptrn[2]));   
-            lfsr_trial[3][kl+1] = (lfsr_trial[3][kl]<<1)+(^(lfsr_trial[3][kl]&LFSR_ptrn[3]));   
-            lfsr_trial[4][kl+1] = (lfsr_trial[4][kl]<<1)+(^(lfsr_trial[4][kl]&LFSR_ptrn[4]));   
-            lfsr_trial[5][kl+1] = (lfsr_trial[5][kl]<<1)+(^(lfsr_trial[5][kl]&LFSR_ptrn[5]));   
+            lfsr_trial[0][kl+1] <= (lfsr_trial[0][kl]<<1)+(^(lfsr_trial[0][kl]&LFSR_ptrn[0]));   
+            lfsr_trial[1][kl+1] <= (lfsr_trial[1][kl]<<1)+(^(lfsr_trial[1][kl]&LFSR_ptrn[1]));   
+            lfsr_trial[2][kl+1] <= (lfsr_trial[2][kl]<<1)+(^(lfsr_trial[2][kl]&LFSR_ptrn[2]));   
+            lfsr_trial[3][kl+1] <= (lfsr_trial[3][kl]<<1)+(^(lfsr_trial[3][kl]&LFSR_ptrn[3]));   
+            lfsr_trial[4][kl+1] <= (lfsr_trial[4][kl]<<1)+(^(lfsr_trial[4][kl]&LFSR_ptrn[4]));   
+            lfsr_trial[5][kl+1] <= (lfsr_trial[5][kl]<<1)+(^(lfsr_trial[5][kl]&LFSR_ptrn[5]));   
             $display("trials %d %h %h %h %h %h %h    %h",  kl,
 				 lfsr_trial[0][kl+1],
 				 lfsr_trial[1][kl+1],
@@ -111,7 +127,7 @@ module top_level5_sol(
             $display("mm = %d  lfsr_trial[mm] = %h, LFSR[6] = %h",
 			     mm, lfsr_trial[mm][6], LFSR[6]); 
 		    if(lfsr_trial[mm][6] == LFSR[6]) begin
-			  foundit = mm;
+			  foundit <= mm;
 			  $display("foundit = %d LFSR[6] = %h",foundit,LFSR[6]);
             end
 		  end //:ureka_loop
@@ -123,13 +139,13 @@ module top_level5_sol(
 		  end // end of step 2
 		  						   
 		  else if (step == 4) begin 
-		    LFSR[jm+1] = (LFSR[jm]<<1)+(^(LFSR[jm]&LFSR_ptrn[foundit]));
+		    LFSR[jm+1] <= (LFSR[jm]<<1)+(^(LFSR[jm]&LFSR_ptrn[foundit]));
           for(int mn=7;mn<64-7;mn++) 
 				begin
-				raddr = 64 + mn - 7;
-				#10ns;
-				data_in = data_out^{2'b0,LFSR[mn-7]};
-			$display("%dth core = %h LFSR = %h",mn,data_out,LFSR[mn-7]);
+				raddr <= 64 + mn - 7;
+				waddr <= mn - 7;
+				data_out <= data_in^{2'b0,LFSR[mn-7]};
+			//$display("%dth core = %h LFSR = %h",mn,data_out,LFSR[mn-7]);
 				
 		 /*	
 			 :first_core_write
@@ -140,23 +156,23 @@ module top_level5_sol(
 				end 
          #10ns;
          for(km=0; km<64; km++) begin
-				raddr = km;
+				raddr <= km;
 				#10ns;
-            if(data_out==8'h5f) continue;
+            if(data_in==8'h5f) continue;
             else break;  
 				/*
             if(dm1.core[km]==8'h5f) continue;
             else break;  
 				*/
           end     
-          $display("underscores to %d th",km);
+          //$display("underscores to %d th",km);
           for(int kl=0; kl<64; kl++) 
 			 begin
-			 raddr = kl + km;
-			 waddr = kl;
+			 raddr <= kl + km;
+			 waddr <= kl;
 			 #10ns;
-			 data_in = data_out;
-		    $display("%dth core = %h",kl,data_in);
+			 data_out <= data_in;
+		    //$display("%dth core = %h",kl,data_in);
 			 
 			 /*
             dm1.core[kl] = dm1.core[kl+km];
@@ -171,11 +187,12 @@ module top_level5_sol(
 				raddr = i + 71;
 				waddr = i;
 				#10ns;
-				data_in = {2'b0, LFSR[i]} ^ data_out;
+				data_out <= {2'b0, LFSR[i]} ^ data_in;
 			end // end of test loop
 		end // end of step 4
 	  end   //:loop2
-    end //:init_loop
+	 end // loop 2
+		
   end  //:clock_loop
 
   //initial 
